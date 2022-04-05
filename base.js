e56862b0dbf927fda1760b32b9bbb2d25bb25c5b
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-04-05 01:28:57 -0400

    23

--- base.js
+++ base.js
@@ -9,43 +9,40 @@ const db = async () => {
     },
   });
   await client.connect();
-
-  async function getUsuarios() {
-    try {
-      const res = await client.query("SELECT * from usuarios");
-  await client.end();
-  return res.rows;
-    } catch (e) {
-      return (e)
-    }
-  }
-  
-
-  //insertar nuevas tareas
-  async function nuevoServicio(username, contraseña, email, fechaCreacion) {
-    try {
-      const result = await client.query(
-        `INSERT INTO usuarios(username,contraseña,email,fechaCreacion) values('${username}',
-            '${contraseña}','${email}', '${fechaCreacion}') RETURNING*;`
-      );
-      return result.rows;
-    } catch (e) {
-      return e;
-    }
-  }
-
-  //borrar tareas del documento
-  async function deleteServicio(id) {
-    try {
-      const result = await client.query(
-        `DELETE FROM usuarios WHERE id='${id}'`
-      );
-      return result.rowCount;
-    } catch (e) {
-      return e;
-    }
-  }
 };
+
+async function getUsuarios() {
+  try {
+    const res = await client.query("SELECT * from usuarios");
+    await client.end();
+    return res.rows;
+  } catch (e) {
+    return e;
+  }
+}
+
+//insertar nuevas tareas
+async function nuevoServicio(username, contraseña, email, fechaCreacion) {
+  try {
+    const result = await client.query(
+      `INSERT INTO usuarios(username,contraseña,email,fechaCreacion) values('${username}',
+            '${contraseña}','${email}', '${fechaCreacion}') RETURNING*;`
+    );
+    return result.rows;
+  } catch (e) {
+    return e;
+  }
+}
+
+//borrar tareas del documento
+async function deleteServicio(id) {
+  try {
+    const result = await client.query(`DELETE FROM usuarios WHERE id='${id}'`);
+    return result.rowCount;
+  } catch (e) {
+    return e;
+  }
+}
 
       
 module.exports = db, getUsuarios, nuevoServicio, deleteServicio;
